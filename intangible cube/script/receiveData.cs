afbbea7c57eb43b3a8427e4905ae0b87cfce1fd0
jiaolyulu <54914804+jiaolyulu@users.noreply.github.com>
2021-07-26 09:28:32 -0400

    Update receiveData.cs

--- intangible cube/script/receiveData.cs
+++ intangible cube/script/receiveData.cs
@@ -6,11 +6,8 @@ using System.Threading;
 using System.Collections.Generic;
 public class receiveData : MonoBehaviour
 {
- 
-    public string state;
     public int dist=0;
     SerialPort stream = new SerialPort("COM11", 115200); //Set the port (com4) and the baud rate (9600, is standard on most devices)
-    float[] lastRot = { 0, 0, 0 }; //Need the last rotation to tell how far to spin the camera
     public int[] data;
 
     void Start()
@@ -33,49 +30,10 @@ public class receiveData : MonoBehaviour
                 data[nowindex - 2] = int.Parse(info[index * 2 + 1]);
 
             }
-           // Debug.Log(info);
+          
         }
-        //switch (value)
-        //{
-        //    case "System error":
-        //        state = "System error";
-        //        break;
-        //    case "ECE failure":
-        //        state = "ECE failure";
-        //        break;
-        //    case "No convergence":
-        //        state = "No convergence";
-        //        break;
-        //    case "Ignoring range":
-        //        state = "Ignoring range";
-        //        break;
-        //    case "Signal/Noise error":
-        //        state = "Signal/Noise error";
-        //        break;
-        //    case "Raw reading underflow":
-        //        state = "Raw reading underflow";
-        //        break;
-        //    case "Raw reading overflow":
-        //        state = "Raw reading overflow";
-        //        break;
-        //    case "Range reading underflow":
-        //        state = "Range reading underflow";
-        //        break;
-        //    case "Range reading overflow":
-        //        state = "Range reading overflow";
-        //        break;
-        //    default:
-        //        state = "Number";
-        //        break;
-        //}
+      
         
-    }
-
-    void OnGUI()
-    {
-        string newString = "Connected: " + transform.rotation.x + ", " + transform.rotation.y + ", " + transform.rotation.z;
-        GUI.Label(new Rect(10, 10, 300, 100), newString); //Display new values
-                                                          // Though, it seems that it outputs the value in percentage O-o I don't know why.
     }
     void OnApplicationQuit()
     {
